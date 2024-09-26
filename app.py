33f7847923a98c0ca4c517227a49e1aeb6fb36d8
Fahith KRM <141358141+FahithKRM@users.noreply.github.com>
2024-09-27 02:35:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-app.home() :
+def home() :
   return render_template ('index.html')
 
-if __name__ == __main__ :
+if __name__ == '__main__' :
   app.run(debug = True)
